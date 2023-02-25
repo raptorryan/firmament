a65d8cd6ab8e73f9ce8a2bf28c9461cebee99bd4
Jonathan Caprell <jbcaprell@diacritical.net>
2023-02-25 08:30:00 -0500

    Create `:adam` `v0.1.0` app release

--- .formatter.exs
+++ .formatter.exs
@@ -1,1 +1,6 @@
-[force_do_end_blocks: true, inputs: ["{.formatter,mix}.exs"], line_length: 80]
+[
+  force_do_end_blocks: true,
+  inputs: ["{.formatter,mix}.exs"],
+  line_length: 80,
+  subdirectories: ["app/*"]
+]
