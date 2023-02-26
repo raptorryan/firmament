a9a8a630497c63047982c7c964e03d4d3734dc61
Jonathan Caprell <jbcaprell@diacritical.net>
2023-02-26 17:00:00 -0500

    Create `v0.8.0` release

--- mix.exs
+++ mix.exs
@@ -77,7 +77,7 @@ defmodule Firmament.MixProject do
       "app"
 
       iex> project()[:version]
-      "0.7.1"
+      "0.8.0"
 
   """
   @doc since: "0.1.0"
@@ -107,7 +107,7 @@ defmodule Firmament.MixProject do
         ]
       ],
       start_permanent: Mix.env() == :prod,
-      version: "0.7.1"
+      version: "0.8.0"
     ]
   end
 end
